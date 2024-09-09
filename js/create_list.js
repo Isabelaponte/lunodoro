da83f9cf2e925ee0088dcca3acae85f3729f319e
Isabela de Ponte <isabelapnt01@gmail.com>
2024-09-08 22:05:28 -0300

    feat: limite de 3 listas

--- js/create_list.js
+++ js/create_list.js
@@ -1,23 +1,23 @@
 const saveListsToLocal = (lists) => {
-    localStorage.setItem("taskLists", JSON.stringify(lists));
-    renderLists();
-  };
-  
-  const getListsFromLocal = () => {
-    const storedLists = localStorage.getItem("taskLists");
-    return storedLists ? JSON.parse(storedLists) : [];
-  };
-  
-  const renderLists = () => {
-    const lists = getListsFromLocal();
-    const section = document.querySelector("section");
-    section.innerHTML = "";
-  
-    lists.forEach((list, index) => {
-      const card = document.createElement("div");
-      card.classList.add("card");
-  
-      card.innerHTML = `
+  localStorage.setItem("taskLists", JSON.stringify(lists));
+  renderLists();
+};
+
+const getListsFromLocal = () => {
+  const storedLists = localStorage.getItem("taskLists");
+  return storedLists ? JSON.parse(storedLists) : [];
+};
+
+const renderLists = () => {
+  const lists = getListsFromLocal();
+  const section = document.querySelector("section");
+  section.innerHTML = "";
+
+  lists.forEach((list, index) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.innerHTML = `
               <div class="div-list">
                   <div class="tag-progress ${list.status}">${list.statusLabel}</div>  
                   <div class="more-options">
@@ -34,84 +34,88 @@ const saveListsToLocal = (lists) => {
               <p class="list-type">Tipo: ${list.type}</p>
               <p class="date">Criada em ${list.date}</p>
           `;
-      section.appendChild(card);
-    });
-  
-    document.querySelectorAll(".edit-button").forEach((button) => {
-      button.addEventListener("click", handleEdit);
-    });
-  
-    document.querySelectorAll(".delete-button").forEach((button) => {
-      button.addEventListener("click", handleDelete);
-    });
-  };
-  
-  const taskForm = document.querySelector("#taskForm");
-  
-  let currentEditIndex = null;
-  
-  taskForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-  
-    const taskName = document.querySelector("#taskName").value;
-    const taskDescription = document.querySelector("#taskDescription").value;
-    const listType = document.querySelector("#listType").value;
-  
-    if (currentEditIndex === null) {
-      const newList = {
-        id: Date.now(),
-        name: taskName,
-        description: taskDescription || "Sem descrição",
-        type: listType,
-        status: "em_progresso",
-        statusLabel: "Em progresso",
-        date: new Date().toLocaleDateString(),
-        tarefas: [],
-      };
-  
-      const lists = getListsFromLocal();
-      lists.push(newList);
-      saveListsToLocal(lists);
-    } else {
-      const lists = getListsFromLocal();
-  
-      lists[currentEditIndex] = {
-        ...lists[currentEditIndex],
-        name: taskName,
-        description: taskDescription,
-        type: listType,
-      };
-  
-      saveListsToLocal(lists);
-      currentEditIndex = null;
+    section.appendChild(card);
+  });
+
+  document.querySelectorAll(".edit-button").forEach((button) => {
+    button.addEventListener("click", handleEdit);
+  });
+
+  document.querySelectorAll(".delete-button").forEach((button) => {
+    button.addEventListener("click", handleDelete);
+  });
+};
+
+const taskForm = document.querySelector("#taskForm");
+
+let currentEditIndex = null;
+
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const taskName = document.querySelector("#taskName").value;
+  const taskDescription = document.querySelector("#taskDescription").value;
+  const listType = document.querySelector("#listType").value;
+
+  if (currentEditIndex === null) {
+    const lists = getListsFromLocal();
+    if (lists.length >= 3) {
+      alert("Limite de 3 listas excedido! Faça login para criar mais listas.");
+      return;
     }
-  
-    modal.style.display = "none";
-    taskForm.reset();
-  });
-  
-  const handleEdit = (event) => {
-    const index = event.target.closest("button").getAttribute("data-index");
-    const lists = getListsFromLocal();
-    const listToEdit = lists[index];
-  
-    document.querySelector("#taskName").value = listToEdit.name;
-    document.querySelector("#taskDescription").value = listToEdit.description;
-    document.querySelector("#listType").value = listToEdit.type;
-  
-    currentEditIndex = index;
-  
-    modal.style.display = "flex";
-  };
-  
-  const handleDelete = (event) => {
-    const index = event.target.closest("button").getAttribute("data-index");
-    const lists = getListsFromLocal();
-    lists.splice(index, 1);
+
+    const newList = {
+      id: Date.now(),
+      name: taskName,
+      description: taskDescription || "Sem descrição",
+      type: listType,
+      status: "em_progresso",
+      statusLabel: "Em progresso",
+      date: new Date().toLocaleDateString(),
+      tarefas: [],
+    };
+
+    lists.push(newList);
     saveListsToLocal(lists);
-  };
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    renderLists();
-  });
-  
+  } else {
+    const lists = getListsFromLocal();
+
+    lists[currentEditIndex] = {
+      ...lists[currentEditIndex],
+      name: taskName,
+      description: taskDescription,
+      type: listType,
+    };
+
+    saveListsToLocal(lists);
+    currentEditIndex = null;
+  }
+
+  modal.style.display = "none";
+  taskForm.reset();
+});
+
+const handleEdit = (event) => {
+  const index = event.target.closest("button").getAttribute("data-index");
+  const lists = getListsFromLocal();
+  const listToEdit = lists[index];
+
+  document.querySelector("#taskName").value = listToEdit.name;
+  document.querySelector("#taskDescription").value = listToEdit.description;
+  document.querySelector("#listType").value = listToEdit.type;
+
+  currentEditIndex = index;
+
+  modal.style.display = "flex";
+};
+
+const handleDelete = (event) => {
+  const index = event.target.closest("button").getAttribute("data-index");
+  const lists = getListsFromLocal();
+  lists.splice(index, 1);
+  saveListsToLocal(lists);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderLists();
+});
