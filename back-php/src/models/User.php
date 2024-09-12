66949bf0530eac332c308bfd3f8a213eed3af3b6
Ryan Lucas <ryanlucaspatron@gmail.com>
2024-09-11 22:27:13 -0300

    refactor: atualização nas classes user e user validator

--- back-php/src/models/User.php
+++ back-php/src/models/User.php
@@ -1,13 +1,59 @@
 <?php
-class User {
-    public $id;
-    public $nome;
-    public $email;
-    public $senha;
 
-    public function __construct($nome, $email, $senha) {
-        $this->nome = $nome;
+class User
+{
+    private $id;
+    private $name;
+    private $email;
+    private $password;
+    private $dt_account_creation;
+
+    public function __construct($id = null, $name = null, $email, $password, $dt_account_creation = null)
+    {
+        $this->id = $id;
+        $this->name = $name;
         $this->email = $email;
-        $this->senha = $senha;
+        $this->password = $password;
+        $this->dt_account_creation = $dt_account_creation;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUserName()
+    {
+        return $this->name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getDtAccountCreation() {
+        return $this->dt_account_creation;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function setPassword($password){
+        $this->password = $password;
     }
 }
+
+?>
--- back-php/src/validators/UserValidator.php
+++ back-php/src/validators/UserValidator.php
@@ -1,20 +1,37 @@
 <?php
-class UserValidator {
-    public static function validate($nome, $email, $senha) {
+
+class UserValidator
+{
+    public static function validate($name, $email, $password, $dt_account_creation = null)
+    {
         $errors = [];
 
-        if (empty($nome)) {
-            $errors[] = "Nome é obrigatório.";
+        // Validar Nome
+        if (empty($name) || !is_string($name)) {
+            $errors[] = "O nome deve ser uma string não vazia.";
         }
 
+        // Validar Email
         if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors[] = "Email inválido.";
+            $errors[] = "Formato de email inválido.";
         }
 
-        if (empty($senha)) {
-            $errors[] = "Senha é obrigatória.";
+        // Validar Senha
+        if (empty($password)) {
+            $errors[] = "A senha é obrigatória.";
+        }
+
+        // Validar Data de Criação da Conta
+        if ($dt_account_creation !== null && !self::validateDate($dt_account_creation)) {
+            $errors[] = "A data de criação da conta deve estar no formato Y-m-d H:i:s.";
         }
 
         return $errors;
+    }
+
+    private static function validateDate($date)
+    {
+        $d = DateTime::createFromFormat('Y-m-d H:i:s', $date);
+        return $d && $d->format('Y-m-d H:i:s') === $date;
     }
 }
