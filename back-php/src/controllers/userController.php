bff287f7b75bef62740fd4fc6f899d2c25997b5a
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2024-11-08 15:23:02 -0300

    refactor: apply object-oriented principles for improved readability and code organization

--- back-php/src/controllers/userController.php
+++ back-php/src/controllers/userController.php
@@ -1,55 +1,91 @@
 <?php
 
-require_once(__DIR__ . '/../validators/MethodValidator.php');
 require_once(__DIR__ . '/../services/UserService.php');
 require_once(__DIR__ . '/../config/utils.php');
+require_once(__DIR__ . '/../models/User.php');
 
 header("Access-Control-Allow-Methods: POST, GET");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+class UserController {
 
-if (validatorMethodServer('POST')) {
-    if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])) {
-        try {
-            $response = UserService::saveUser(
-                $_POST["name"],
-                $_POST["email"],
-                $_POST["password"]
-            );
-            output(201, $response);
-        } catch (Exception $e) {
-            output($e->getCode(), ["error" => $e->getMessage()]);
+    public function handleRequest(string $method): void
+{
+    try {
+        switch (strtoupper($method)) {
+            case 'POST':
+                $name = $_POST['name'] ?? null;
+                $email = $_POST['email'] ?? null;
+                $password = $_POST['password'] ?? null;
+
+                if ($name && $email && $password) {
+                    $this->createUser($name, $email, $password);
+                } elseif ($email && $password) {
+                    $this->loginUser($email, $password);
+                } else {
+                    $this->output(400, ["error" => "Parâmetros ausentes"]);
+                }
+                break;
+
+            case 'GET':
+                $id = $_GET['id'] ?? null;
+
+                if ($id) {
+                    $this->getUserData($id);
+                } else {
+                    $this->output(400, ["error" => "Parâmetros ausentes"]);
+                }
+                break;
+
+            default:
+                $this->output(405, ["error" => "Método não permitido"]);
+                break;
         }
-    } 
-    elseif (isset($_POST['email']) && isset($_POST['password']) && !isset($_POST['name'])) {
-        try {
-            $response = UserService::getUser(
-                $_POST["email"],
-                $_POST["password"]
-            );
-            output(200, $response);
-        } catch (Exception $e) {
-            output($e->getCode(), ["error" => $e->getMessage()]);
-        }
-    } else {
-        output(400, ["error" => "Parâmetros ausentes"]);
+    } catch (Exception $e) {
+        $this->output($e->getCode(), ["error" => $e->getMessage()]);
     }
 }
 
-if(validatorMethodServer(("GET"))){
-    if(isset($_GET['id'])){
+    private function createUser($name, $email, $password): void
+    {
         try {
-            $response = UserService::getMyData(
-                $_GET["id"]
-            );
-            output(200, $response);
+            $user = new User($email, $password, $name);
+            $response = UserService::saveUser($user);
+            $this->output(201, $response);
         } catch (Exception $e) {
-            output($e->getCode(), ["error" => $e->getMessage()]);
+            $this->output($e->getCode(), ["error" => $e->getMessage()]);
         }
-    }else {
-        output(400, ["error" => "Parâmetros ausentes"]);
+    }
+
+    private function loginUser($email, $password): void
+    {
+        try {
+            $user = new User($email, $password);
+            $response = UserService::getUser($user);
+            $this->output(200, $response);
+        } catch (Exception $e) {
+            $this->output($e->getCode(), ["error" => $e->getMessage()]);
+        }
+    }
+
+    private function getUserData($id): void
+    {
+        try {
+            $response = UserService::getMyData($id);
+            $this->output(200, $response);
+        } catch (Exception $e) {
+            $this->output($e->getCode(), ["error" => $e->getMessage()]);
+        }
+    }
+
+    private function output(int $statusCode, array $response): void
+    {
+        http_response_code($statusCode);
+        echo json_encode($response);
     }
 }
 
-output(400, ["error" => "Tipo de requisicao nao aceita"]);
+$controller = new UserController();
+$controller->handleRequest($_SERVER['REQUEST_METHOD']);
